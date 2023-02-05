c28b43b17c653be2ffc37d7aa2735dcd50d331a2
Dominik Pielak <dominikpielak@gmail.com>
2023-02-05 13:02:31 +0100

    Input - support for pad in MainWindow

--- Assets/Scripts/UI/Windows/MainWindow/MainWindow.cs
+++ Assets/Scripts/UI/Windows/MainWindow/MainWindow.cs
@@ -1,10 +1,23 @@
 ﻿using BoxColliders.Project;
 using PDGames.UserInterface;
+using UnityEngine;
 
 namespace BoxColliders.Windows
 {
     public sealed class MainWindow : BaseWindow<MainWindowView>
     {
+        #region MONO BEHAVIOUR
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire3"))
+            {
+                OnPlayPressed();
+            }
+        }
+        
+        #endregion
+        
         #region INPUT HANDLING
         
         public void OnPlayPressed()
